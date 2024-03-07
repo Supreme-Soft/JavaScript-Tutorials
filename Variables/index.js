77e8f1e371dce9ea9325061e7eaeee847ac271c2
Supreme-Soft <supremesoft99@gmail.com>
2024-03-07 21:02:18 +0545

    Update

--- Variables/index.js
+++ Variables/index.js
@@ -1,2 +1,24 @@
-const heading = document.getElementById("heading");
-heading.innerText = "Hello JavaScript";
+// Available on GitHub - Check Description
+
+// Previous example
+// console.log("Hello World");
+
+// Timestamp 0:10
+// Applied variable and updated message
+// let message = "Hello World";
+//     message = "Hello JavaScript";
+
+// console.log(message);
+
+
+// Timestamp 0:11
+
+let firstName = "John";
+let lastName = "Doe";
+let age = 30;
+let favoriteColor = "blue";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(favoriteColor);
